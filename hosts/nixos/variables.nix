0325a197cae8a57a283998f11e022216b1773c58
installer <installer@gmail.com>
2024-11-19 14:41:32 -0600

    added my hostname, both intel and nvidia busid

---
+++ hosts/nixos/variables.nix
@@ -0,0 +1,16 @@
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "fit-s-u-m";
+  gitEmail = "nehemiahmaths@gmai.com";
+
+  # Hyprland Settings
+  extraMonitorSettings = "";
+
+  # Waybar Settings
+  clock24h = false;
+
+  # Program Options
+  browser = "brave"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "kitty"; # Set Default System Terminal
+  keyboardLayout = "us";
+}
